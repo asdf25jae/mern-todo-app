42abde950c44109827fe63137532daaa0578b95e
Jae <asdf25jae@gmail.com>
2020-11-19 21:39:37 -0800

    first commit for mern todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"; // import React, and Component class
+import "bootstrap/dist/css/bootstrap.min.css"; // imports bootstrap into play
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// note that App.js is the entry point for the entire react application
+
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      
+    );
+  }
 }
 
 export default App;
