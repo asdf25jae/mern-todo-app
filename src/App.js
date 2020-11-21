ab297e930df23ef53d98dd980557125b70f4a0e2
Jae <asdf25jae@gmail.com>
2020-11-20 22:47:00 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"; // import React, and Component class
 import "bootstrap/dist/css/bootstrap.min.css"; // imports bootstrap into play
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"; 
 
 // note that App.js is the entry point for the entire react application
 
@@ -7,11 +8,14 @@ import "bootstrap/dist/css/bootstrap.min.css"; // imports bootstrap into play
 class App extends Component {
   render() {
     return (
-      
+      <Router>
+        <Route path="/" exact component = { TodosList } />
+        <Route path="/edit/:id" component = { EditTodo } />
+        <Route path="/create" component= { CreateTodo } />
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
         </div>
-      
+      </Router>
     );
   }
 }
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+// export default class, meaning that this TodosList class 
+// is meant to be exported 
+export default class TodosList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to Todos List Component!!</p>
+            </div>
+        )
+    }
+}
