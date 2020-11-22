6b4815816c87525b785167c37c4ebefa9155e0f3
Jae <asdf25jae@gmail.com>
2020-11-22 00:18:14 -0800

    added axios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react"; // import React, and Component class
 import "bootstrap/dist/css/bootstrap.min.css"; // imports bootstrap into play
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"; 
 
-// note that App.js is the entry point for the entire react application
+// note that index.js is the entry point for the entire react application
 
+// and index.js is calling App as <App />
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
@@ -14,8 +17,8 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={ logo } width="30" height="30" alt="CodingTheSmartWay.com" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
             <div className="collpase navbar-collapse">
