9fd2c9c5e813bdece40edaa988c39ff1d3221184
Jae <asdf25jae@gmail.com>
2020-11-22 01:02:12 -0800

    connected front end to backend using axios

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+
+const Todo = props => (
+    <tr>
+        <td>{props.todo.todo_description}</td>
+        <td>{props.todo.todo_responsible}</td>
+        <td>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 // export default class, meaning that this TodosList class 
 // is meant to be exported 
 export default class TodosList extends Component {
-    render() {
+
+    constructor(props) {
+        super(props); // to inherit methods of parent class
+        // must call super(props) in constructor (init function) 
+        this.state = {todos: []};
+    }
+
+    //
+    componentDidMount() {
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({ todos: response.data });
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }
+
+    todoList() {
+        // returns a mapped array of <Todo todo=<{currentTodo} key={i} /> 
+        return this.state.todos.map(function(currentTodo, i){
+            return <Todo todo={currentTodo} key={i} />;
+        })
+    }
+
+    render() { // what the React Component Renders (returns) 
         return (
             <div>
-                <p>Welcome to Todos List Component!!</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }} >
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.todoList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
